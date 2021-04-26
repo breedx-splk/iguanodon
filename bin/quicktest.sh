945f35089ab95ba900a980b878ce8108dd731d00
Jason Plumb <jplumb@splunk.com>
2021-04-26 15:53:20 -0700

    quicker tests

--- bin/quicktest.sh
+++ bin/quicktest.sh
@@ -5,3 +5,8 @@ echo quick test here
 echo here is the github workspace dir
 ls -l /github/workspace/
 
+echo making a file in the workspace
+echo TEST123 > /github/workspace/results/FLIMFLAM
+echo $?
+echo lets check to see if it worked
+echo ls -l /github/workspace/results/
